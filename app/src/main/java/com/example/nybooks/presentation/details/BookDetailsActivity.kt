691a711cb6212f4bb6491fc0d4ecf84aa0e2f081
Leonardo-Sangali <leonardo.dalmonte.s@gmail.com>
2022-03-27 14:06:03 -0300

    Finished Project

---
+++ app/src/main/java/com/example/nybooks/presentation/details/BookDetailsActivity.kt
@@ -0,0 +1,37 @@
+package com.example.nybooks.presentation.details
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.example.nybooks.R
+import com.example.nybooks.databinding.ActivityBookDetailsBinding
+import com.example.nybooks.presentation.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_book_details.*
+import kotlinx.android.synthetic.main.include_toolbar.*
+
+class BookDetailsActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityBookDetailsBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_book_details)
+
+        setupToolbar(binding.includeToolbarDetails.toolbarMain, R.string.book_details_title, true)
+        bookDetailsTitle.text = intent.getStringExtra(EXTRA_TITLE)
+        bookDetailsDescription.text = intent.getStringExtra(EXTRA_DESCRIPTION)
+
+    }
+
+    companion object {
+        private const val EXTRA_TITLE = "EXTRA_TITLE"
+        private const val EXTRA_DESCRIPTION = "EXTRA_DESCRIPTION"
+
+        fun getStartIntent(context: Context, title: String, description: String): Intent {
+            return Intent(context, BookDetailsActivity::class.java). apply {
+                putExtra(EXTRA_TITLE, title)
+                putExtra(EXTRA_DESCRIPTION, description)
+            }
+        }
+
+    }
+}
